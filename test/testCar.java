cfa6567d8e06f98cbc66e4ceb32021505d0e6459
Cronvall <47293083+Cronvall@users.noreply.github.com>
2021-11-12 15:26:38 +0100

    Tests
    Tests for Saab95.java & Volvo240.java

--- test/testCar.java
+++ test/testCar.java
@@ -1,2 +1,58 @@
+import org.junit.jupiter.api.*;
+import java.awt.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class testCar {
+
+    @Test
+    void testTurnLeft(){
+        Volvo240 volvo = new Volvo240(2,420,Color.red);
+
+        assertEquals(1, volvo.getDirection(), "Assert start direction is forward (1)");
+        volvo.turnLeft();
+        assertEquals(4,volvo.getDirection(), "Asserts that turn left on exception case");
+        volvo.turnLeft();
+        assertEquals(3,volvo.getDirection(), "Asserts that turn left on normal case");
+    }
+
+    @Test
+    void testTurnRight(){
+        Saab95 saab = new Saab95(2,16,Color.CYAN);
+        assertEquals(1, saab.getDirection(), "Asserts start direction is forward (1)");
+        saab.turnRight();
+        assertEquals(2, saab.getDirection(), "Asserts that turn right turns right");
+        saab.turnRight();
+        saab.turnRight();
+        saab.turnRight();
+        assertEquals(1,saab.getDirection(), "Asserts that all rotations went well");
+    }
+
+    @Test
+    void testMovement(){
+        Volvo240 volvo = new Volvo240(4,120,Color.red);
+        assertEquals(0, volvo.getPosition().x,"Asserts starting x position");
+        assertEquals(0, volvo.getPosition().y,"Asserts starting y position");
+
+        //Drive forward
+        volvo.move();
+        System.out.println(volvo.getPosition());
+        assertEquals(0,volvo.getPosition().x, "Assert car stayed put in x-axis");
+        assertEquals(1,volvo.getPosition().y, "Asserts car moved forward");
+        //Drive right
+        volvo.turnRight();
+        volvo.move();
+        assertEquals(1,volvo.getPosition().y, "Asserts car stayed put in y-axis");
+        assertEquals(1,volvo.getPosition().x, "Assert car moved right");
+        //Drive reverse
+        volvo.turnRight();
+        volvo.move();
+        assertEquals(1,volvo.getPosition().x, "Assert car stayed put in x-axis");
+        assertEquals(0, volvo.getPosition().y, "Assert car reversed (y-axis)");
+        //Drive left
+        volvo.turnRight();
+        volvo.move();
+        assertEquals(0,volvo.getPosition().y, "Asserts car stayed put in y-axis");
+        assertEquals(0,volvo.getPosition().x, "Asserts car moved left");
+    }
+
 }
--- test/testSaab95.java
+++ test/testSaab95.java
@@ -1,11 +1,18 @@
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import java.awt.*;
 
 public class testSaab95 {
 
     @Test
-    void isSpeedFactorDouble(){
+    void testSpeedFactor(){
         Saab95 saab = new Saab95(4,150, Color.black);
+        saab.setTurboOff();
+        double desiredVal = 150 * 0.01;
+        assertEquals(desiredVal,saab.speedFactor(), "Test without turbo.");
+        saab.setTurboOn();
+        desiredVal = 150 * 0.01 * 1.3;
+        assertEquals(desiredVal,saab.speedFactor(), "Test with turbo");
     }
 
 }
