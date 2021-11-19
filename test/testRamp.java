3cf80afc82af7564aed268f1791dbddc8eea6358
Cronvall <47293083+Cronvall@users.noreply.github.com>
2021-11-19 14:02:36 +0100

    Ramp class
    contains all non-private methods:
    getAngle()
    raise()
    lower()
    
    Relevant tests have been done in testRamp.java

---
+++ test/testRamp.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testRamp {
+    Ramp ramp = new Ramp();
+
+    @Test
+    void testRampMovement(){
+
+        ramp.raise(50);
+        assertEquals(50, ramp.getAngle());
+
+        ramp.lower(30);
+        assertEquals(20, ramp.getAngle());
+
+        //Since the input is invalid the angle shouldn't change
+        ramp.lower(-100);
+        assertEquals(20, ramp.getAngle());
+        ramp.raise((-100));
+        assertEquals(20, ramp.getAngle());
+
+        //Since the new angle exceeds the desired interval [0,70] it should base to 70
+        ramp.raise(70);
+        assertEquals(70, ramp.getAngle());
+
+        //Since the new angle exceeds the desired interval [0,70] it should base to 0
+        ramp.lower(80);
+        assertEquals(0, ramp.getAngle());
+    }
+}
